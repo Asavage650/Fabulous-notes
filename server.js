f8f6c4b974350638fbe0f5ac5526d60be8356fb7
{alexius <{alexiussavage0@gmail.com}>
2023-06-01 20:07:38 -0700

    fixed files and folders

--- server.js
+++ server.js
@@ -1,25 +1,30 @@
-const fs = require('fs');
-const path = require('path');
+const fs = require("fs");
+const path = require("path");
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3001;
-// const Totalnotes = require('./Develop/db/db.json');
+const Totalnotes = require("./db/db.json");
 
 app.use(express.json());
-app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
 
+app.get("/api/notes", (req, res) => {
+  res.json(Totalnotes.slice(1));
+});
 
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/index.html"));
+});
 
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/notes.html"));
+});
 
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/index.html"));
+});
 
-function newNotes()
-function deleteNotes()
-
-app.deleteNotes()
 app.listen(PORT, () => {
-    console.log(`API server now on port ${PORT}`);
-}); 
-
-
-
+  console.log(`API server now on port ${PORT}`);
+});

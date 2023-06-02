37923e1cf6c588c5fb12b4a8d2c10eb752bfe088
{alexius <{alexiussavage0@gmail.com}>
2023-06-01 21:19:08 -0700

    added func to delete notes

--- server.js
+++ server.js
@@ -3,14 +3,14 @@ const path = require("path");
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3001;
-const Totalnotes = require("./db/db.json");
+const totalNotes = require("./db/db.json");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 app.get("/api/notes", (req, res) => {
-  res.json(Totalnotes.slice(1));
+  res.json(totalNotes.slice(1));
 });
 
 app.get("/", (req, res) => {
@@ -36,13 +36,30 @@ function createNewNotes(body, notesArray) {
   notesArray.push(newNotes);
   fs.writeFileSync(
     path.json(__dirname, "./db/db.json"),
-    JSON.stringify(notesArray, NULL, 2)
+    JSON.stringify(notesArray, null, 2)
   );
   return newNotes;
 }
 app.post("/api/notes", (req, res) => {
-  const newNotes = createNewNotes(req.body, Totalnotes);
+  const newNotes = createNewNotes(req.body, totalNotes);
   res.json(newNotes);
+});
+
+function deleteNote(id, notesArray) {
+  for (let i = 0; i < notesArray.length; i++) {
+    let note = notesArray[i];
+    if (note.id == id) {
+      notesArray.splice(i, 1);
+      fs.writeFileSync(
+        path.join(__dirname, "./db/db.json"),
+        JSON.stringify(notesArray, null, 2)
+      );
+    }
+  }
+}
+app.delete("/api/notes/:id", (req, res) => {
+  deleteNote(req.params.id, totalNotes);
+  res.json(true);
 });
 app.listen(PORT, () => {
   console.log(`API server now on port ${PORT}`);

7a422bf9d0acdfc26a1641d63691724532a19ee6
{alexius <{alexiussavage0@gmail.com}>
2023-06-05 14:40:50 -0700

    trying to fix bug

--- server.js
+++ server.js
@@ -21,9 +21,9 @@ app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "./public/notes.html"));
 });
 
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "./public/index.html"));
-});
+// app.get("/", (req, res) => {
+//   res.sendFile(path.join(__dirname, "./public/index.html"));
+// });
 
 function createNewNotes(body, notesArray) {
   const newNotes = body;
